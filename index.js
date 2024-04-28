311eaee4826c4fb249702581d32c6c8ae5ff7791
Naimur00 <aminaim4u@gmail.com>
2024-04-28 10:50:50 -0600

    Update method implement

--- index.js
+++ index.js
@@ -29,22 +29,53 @@ async function run() {
     const craftCollection = client.db("craftDB").collection("craft")
 
     app.get('/craft', async (req, res) => {
-        const cursor = craftCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     app.post('/craft', async (req, res) => {
-        const newCraft = req.body;
-        console.log(newCraft);
-        const result = await craftCollection.insertOne(newCraft);
-        res.send(result);
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
     })
 
     app.delete('/craft/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/craft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.put('/craft/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedCraft = req.body;
+
+      const craft = {
+          $set: {
+            name: updatedCraft.name,
+            subName: updatedCraft.subName,
+            image: updatedCraft.image,
+            price: updatedCraft.price,
+            time: updatedCraft.time,
+            rating: updatedCraft.rating,
+            description: updatedCraft.description,
+            stock: updatedCraft.stock,
+            customizable: updatedCraft.customizable
+          }
+      }
+
+      const result = await craftCollection.updateOne(filter, craft, options);
       res.send(result);
   })
 
@@ -60,9 +91,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Craft making server is running')
+  res.send('Craft making server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Craft Server is running on port: ${port}`)
+  console.log(`Craft Server is running on port: ${port}`)
 })
